8ee9392303012da4f6beb4306cc29abab9610acd
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-09-08 18:56:50 -0300

    feat: carousel responsivity finished

--- src/app/components/space/page.tsx
+++ src/app/components/space/page.tsx
@@ -15,53 +15,65 @@ export default function Space() {
       block: 'nearest',
       inline: 'center',
     })
-  })
+  }, [value])
 
   function handleScrollClickRight() {
     value < 4 ? setValue(value + 1) : setValue(0)
 
-    console.log(`Dentro do escopo de uma função do componente: ${value}`)
+    // selectedRef.current?.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'nearest',
+    //   inline: 'center',
+    // })
+
+    // console.log(`Dentro do escopo de uma função do componente: ${value}`)
   }
 
   function handleScrollClickLeft() {
     value > 0 ? setValue(value - 1) : setValue(4)
 
-    console.log(`Dentro do escopo de uma função do componente: ${value}`)
+    // selectedRef.current?.scrollIntoView({
+    //   behavior: 'smooth',
+    //   block: 'nearest',
+    //   inline: 'center',
+    // })
+
+    // console.log(`Dentro do escopo de uma função do componente: ${value}`)
   }
 
-  console.log(`No escopo do componente:  ${value}`)
+  // console.log(`No escopo do componente:  ${value}`)
 
   const images = [
     {
-      src: 'https://picsum.photos/650/450',
+      src: 'https://picsum.photos/700/500',
       title: 'Lorem Ipsun',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. quam sunt corrupti laborum nobis. Facilis, mollitia. Aspernatur, culpa?',
       id: 0,
     },
     {
-      src: 'https://picsum.photos/650/450',
+      src: 'https://picsum.photos/700/500',
       title: 'Lorem Ipsun',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. quam sunt corrupti laborum nobis. Facilis, mollitia. Aspernatur, culpa?',
       id: 1,
     },
     {
-      src: 'https://picsum.photos/650/450',
+      src: 'https://picsum.photos/700/500',
       title: 'Lorem Ipsun',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. quam sunt corrupti laborum nobis. Facilis, mollitia. Aspernatur, culpa?',
       id: 2,
     },
     {
-      src: 'https://picsum.photos/650/450',
+      src: 'https://picsum.photos/700/500',
       title: 'Lorem Ipsun',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. quam sunt corrupti laborum nobis. Facilis, mollitia. Aspernatur, culpa?',
       id: 3,
     },
     {
-      src: 'https://picsum.photos/650/450',
+      src: 'https://picsum.photos/700/500',
       title: 'Lorem Ipsun',
       content:
         'Lorem ipsum dolor sit, amet consectetur adipisicing elit. quam sunt corrupti laborum nobis. Facilis, mollitia. Aspernatur, culpa?',
@@ -79,41 +91,40 @@ export default function Space() {
           <div className="w-full border border-indigo-950 md:w-4/5"></div>
         </div>
 
-        <div className="relative flex max-w-7xl items-center px-14 py-10">
+        <div className="relative flex max-w-7xl items-center px-2 py-8  sm:px-14 sm:py-10">
           <button
-            className="to-200% absolute bottom-0 left-0 right-auto top-0 z-10 flex w-20 items-center justify-center rounded border-none bg-gradient-to-l from-transparent from-0% to-slate-800 opacity-60 transition-all hover:opacity-100 md:w-32 lg:w-48"
+            className="to-200% absolute bottom-0 left-0 right-auto top-0 z-10 flex items-center justify-center rounded border-none bg-gradient-to-l from-transparent from-0% to-slate-800 opacity-60 transition-all hover:opacity-100 sm:w-32 lg:w-48"
             onClick={handleScrollClickLeft}
           >
-            <RiArrowLeftSLine size={76} className="" />
+            <RiArrowLeftSLine size={52} className="" />
           </button>
           <button
-            className="to-200% absolute bottom-0 left-auto right-0 top-0 z-10 flex w-20 items-center justify-center rounded border-none bg-gradient-to-r from-transparent from-0% to-slate-800 opacity-60 transition-all hover:opacity-100 md:w-32 lg:w-48"
+            className="to-200% absolute bottom-0 left-auto right-0 top-0 z-10 flex items-center justify-center rounded border-none bg-gradient-to-r from-transparent from-0% to-slate-800 opacity-60 transition-all hover:opacity-100 sm:w-32 lg:w-48"
             onClick={handleScrollClickRight}
           >
-            <RiArrowRightSLine size={76} />
+            <RiArrowRightSLine size={52} />
           </button>
 
-          <div className="no-scrollbar flex snap-x flex-row flex-nowrap gap-8 overflow-x-auto overflow-y-hidden">
+          <div className="no-scrollbar flex snap-x flex-row flex-nowrap gap-4 overflow-x-auto overflow-y-hidden sm:gap-8">
             {images.map((image, index) => (
               <div
                 className={
                   index === value
-                    ? 'flex min-w-[600px] flex-col'
-                    : 'flex min-w-[600px] flex-col opacity-50'
+                    ? 'flex min-h-[200px] min-w-[300px] flex-col sm:min-h-[400px] sm:min-w-[450px] xl:min-h-[450px] xl:min-w-[550px]'
+                    : 'flex min-h-[200px] min-w-[300px]  flex-col opacity-50 sm:min-h-[400px] sm:min-w-[450px] xl:min-h-[450px] xl:min-w-[550px]'
                 }
                 key={image.id}
                 ref={index === value ? selectedRef : null}
               >
                 <Image
                   src={image.src}
-                  width={600}
-                  height={400}
+                  width={700}
+                  height={500}
                   alt="teste"
-                  // style={{ width: '100%', height: 'auto' }}
                   className="h-auto w-full rounded-xl"
                 />
 
-                <div className="px-8 pb-8 pt-10 text-indigo-950">
+                <div className="px-6 pt-6 text-indigo-950">
                   <h2 className="mb-1 text-lg font-semibold">{image.title}</h2>
                   <p className="text-sm font-normal">{image.content}</p>
                 </div>

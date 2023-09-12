8a870f386c38f494d9732ec652c2c73243332097
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2023-09-11 23:53:08 -0300

    feat: new footer styles added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,24 +5,34 @@ import Doubts from './Doubts'
 
 export default function Footer() {
   return (
-    <div className="mb-2 mt-32 flex flex-col items-center justify-between rounded-md bg-gradient-to-r from-indigo-950 to-gradientColorBlue px-5 py-8 text-white">
+    <div className="mb-2 mt-32 flex flex-col items-center justify-between rounded-md bg-gradient-to-r from-indigo-950 to-gradientColorBlue px-8 py-8 text-white">
       <Doubts />
       <div className="mt-24 flex w-full justify-between">
-        <div>
-          <h1 className="mb-4 text-lg">EventsSpace</h1>
-          <p className="text-sm font-light text-slate-200">
-            eventsSpace@gmail.com
-          </p>
-          <p className="my-1 text-sm font-light text-slate-200">33912345678</p>
-          <p className="text-sm font-light text-slate-200">
-            Todos os direitos reservados
-          </p>
+        <div className="flex flex-col justify-between">
+          <div>
+            <h1 className="mb-4 text-xl">EventsSpace</h1>
+          </div>
+
+          <div>
+            <p className="text-sm font-light text-slate-200">
+              eventsSpace@gmail.com
+            </p>
+            <p className="my-1 text-sm font-light text-slate-200">
+              33912345678
+            </p>
+            <p className="text-sm font-light text-slate-200">
+              Todos os direitos reservados
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col justify-between">
-          <RxInstagramLogo size={28} className="cursor-pointer" />
-          <AiOutlineFacebook size={28} className="my-4 cursor-pointer" />
-          <TbBrandTiktok size={28} className="cursor-pointer" />
+        <div className="flex flex-col items-center justify-between sm:flex-row sm:items-center">
+          <RxInstagramLogo size={32} className="cursor-pointer md:h-9 md:w-9" />
+          <AiOutlineFacebook
+            size={32}
+            className="mx-4 my-3 cursor-pointer md:mx-6 md:h-9"
+          />
+          <TbBrandTiktok size={32} className="cursor-pointer md:h-9 md:w-9" />
         </div>
       </div>
     </div>
